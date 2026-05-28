3b9a57c502f3df8d7104554d0a26b2c4b0dd469f
miniaturity <miniaturity2.o@gmail.com>
2026-05-28 13:26:07 -0400

    style: button hover fx + animations

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,8 +11,34 @@
 {@render children()}
 
 <style lang="scss">
+	:global(:root) {
+		interpolate-size: allow-keywords;
+	}
+
 	@font-face {
 		font-family: "TE-20";
 		src: url("$lib/assets/fonts/TE-20.ttf");
+	}
+
+	@font-face {
+		font-family: "bjork";
+		src: url("$lib/assets/fonts/bjork.ttf");
+	}
+
+	@font-face {
+		font-family: "HelveticaNeue";
+		src: url("$lib/assets/fonts/HelveticaNeueLight.otf");
+		font-weight: lighter;
+	}
+
+	@font-face {
+		font-family: "HelveticaNeue";
+		src: url("$lib/assets/fonts/HelveticaNeueMedium.otf");
+	}
+
+	@font-face {
+		font-family: "HelveticaNeue";
+		src: url("$lib/assets/fonts/HelveticaNeueThin.otf");
+		font-weight: 100;
 	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-
+    import BARCODE from "$lib/assets/images/barcodeb.jpg";
+    import ARROW from "$lib/assets/images/arrow.png";
 </script>
 
 <div class="page">
@@ -11,8 +12,9 @@
             </header>
 
             <footer>
-                ...a game/front-end dev based in nyc 
+                ...a game/front-end dev 
                 <br />unity, svelte, react, tauri
+                <br />© miniaturity 2026
             </footer>
         </div>
 
@@ -20,6 +22,31 @@
             <button id="projects">
                 <div id="p-content">projects</div>
             </button>
+
+            <button id="contact">
+                <div id="c-content">contact</div>
+            </button>
+        </div>
+
+        <div class="deco-text">
+            <div id="dc-content">
+                <span>mini</span>
+                <span>mini</span>
+            </div>
+        </div>
+
+        <div class="stamp">
+            <div id="s-upper">
+                <div id="su-l">miniaturity</div>
+                <div id="su-r">1C34A3</div>
+            </div>
+            <div id="s-lower">
+                <img alt="" src={BARCODE}/>
+            </div>
+        </div>
+
+        <div class="deco">
+            #175
         </div>
     </div>
 
@@ -37,10 +64,13 @@
         display: flex;
         flex-direction: column;
         align-items: center; justify-content: center;
+
+        background-repeat: repeat;
+        background-size: 400px 400px;
     }
 
     .card {
-        --w: clamp(370px, 45vw, 700px);
+        --w: clamp(370px, 35vw, 700px);
         position: relative;
         aspect-ratio: 6 / 7;
         width: var(--w);
@@ -49,7 +79,8 @@
         background-repeat: repeat;
         font-family: "TE-20";
         
-        overflow: hidden;
+        --button-fs: 1.4rem;
+        forced-color-adjust: none;
     }
 
     .text {
@@ -75,32 +106,103 @@
         color: #000;
         justify-self: end;
         margin: 24px;
+        text-align: justify;
+        text-align-last: justify;
+        width: 170px;
     }
 
     .buttons {
         position: absolute;
         top: 0; left: 0;
         width: 100%; height: 100%;
+
+        pointer-events: none;
+
+        & button {
+            pointer-events: all;
+        }
+    }
+
+    .buttons #projects, .buttons #contact {
+        --tt: 0.3s;
+        transition: all var(--tt) ease-in-out, z-index 0s var(--tt);
+        position: absolute;
+        border: none;
+        font-family: "TE-20";
+        background-position: -50% -50%;
+        color: #fff;
+        display: flex;
+        align-items: end;
+        cursor: pointer;
+        font-size: var(--button-fs);
+        padding: 0;
+        filter: drop-shadow(1px 1px 4px #000);
     }
 
     .buttons #projects {
         --nw: calc(var(--w) / 4);
-        position: absolute;
-        border: none;
-        font-family: "TE-20";
         background-image: url("$lib/assets/images/garden.jpg");
-        background-position: -50% -50%;
-        color: #fff;
+        background-repeat: no-repeat;
+        background-size: 100% 100%;
+        box-sizing: border-box;
         top: 15%; left: 50%;
         aspect-ratio: 3 / 2;
         width: var(--nw); 
-        display: flex;
-        align-items: end;
-        cursor: pointer;
-        font-size: 1.4rem;
-        padding: 0;
+        z-index: 1;
+        
+        
 
         &::after {
+            content: '';
+            position: absolute;
+            left: 0; top: 0;
+            width: 100%; height: 100%;
+            background-color: #fff;
+            opacity: 0.2;
+            transition: opacity 0.2s ease-in-out;
+            box-sizing: border-box;
+        }
+
+        &:hover {
+            left: 0; top: 0;
+            width: 100%; height: 100%;
+            z-index: 1;
+            transition: all var(--tt) ease-in-out, z-index 0s 0s;
+
+            & #p-content {                
+                font-size: 4rem;
+            }
+
+            &::after {
+                opacity: 0;
+                
+            }
+        }
+
+        & #p-content {
+            box-sizing: border-box;
+            width: calc(100% - 8px); height: calc(100% - 8px);
+            margin: 4px;
+            border: 1px solid #fff;
+            display: flex;
+            position: absolute;
+            top: 0; left: 0;
+            padding: 0 6px;
+
+            transition: all var(--tt) ease-in-out; 
+        }
+    }
+
+    .buttons #contact {
+        --nw: calc(var(--w) / 3);
+        background-image: url("$lib/assets/images/subway.jpg");
+        background-size: 100% 100%;
+        top: 20%; left: 70%;
+        aspect-ratio: 3 / 2;
+        width: var(--nw); 
+        z-index: 2;
+     
+         &::after {
             content: '';
             position: absolute;
             left: 0; top: 0;
@@ -111,29 +213,146 @@
         }
 
         &:hover {
+            left: 0; top: 0;
+            width: 100%; height: 100%;
+            z-index: 1;
+
+            & #c-content {
+                font-size: 4rem;
+            }
+
             &::after {
                 opacity: 0;
+                
             }
         }
 
-        & #p-content {
+        & #c-content {
             box-sizing: border-box;
-            width: 100%; height: 100%;
+            width: calc(100% - 8px); height: calc(100% - 8px);
             margin: 4px;
             border: 1px solid #fff;
             display: flex;
-            align-items: end;
-            padding: 4px;
+            position: absolute;
+            top: 0; left: 0;
+            padding: 0 6px;
+
+            transition: all var(--tt) ease-in-out;
         }
     }
+
+    .deco-text {
+        width: 100%; height: 100%;
+        position: absolute;
+        top: 0; left: 0;
+        pointer-events: none;
+        z-index: 0;
+        overflow: hidden;
+    }
+
+    #dc-content {
+        position: absolute;
+        top: 20%;
+        left: -15%;
+
+        display: flex;
+        justify-content: center;
+        font-size: 20rem;
+        color: #ffffff00;
+        opacity: 0.4;
+        -webkit-text-stroke: 2px #fff;
+        overflow: hidden;
+        font-family: "bjork";
+
+        animation: scroll-ltr 12s linear infinite;
+
+        & span {
+            font-size: 20rem;
+            white-space: nowrap;
+            padding-right: 2rem;
+        }
+    }
+
+    @keyframes scroll-ltr {
+        from { transform: translateX(-50%); }
+        to   { transform: translateX(0%);   }
+    }
+
+    .stamp {
+        --nw: 140px;
+        width: var(--nw);
+        aspect-ratio: 8 / 4;
+        border: 3px solid #000;
+        background: #fff;
+        position: absolute;
+        left: -15%; bottom: 20%;
+
+        display: flex;
+        flex-direction: column;
+        font-family: "HelveticaNeue";
+        transform: rotateZ(2deg);
+        z-index: 3;
+    }
+
+    #s-upper {
+        border-bottom: 2px solid #000;
+
+        display: flex;
+        flex-direction: row;
+
+        & div {
+            padding: 4px;
+        }
+
+        & #su-l {
+            border-right: 2px solid #000;
+        }
+
+        & #su-r {
+            font-size: 0.8rem;
+            font-weight: 100;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+    }
+
+    #s-lower {
+        display: flex;
+        justify-content: center; align-items: center;
+        
+
+        & img {
+            object-fit: cover;
+            width: 100%; height: 100%;
+        }
+    }
+
+    .deco {
+        position: absolute;
+        z-index: -1;
+        top: -5%; left: -6%;
+        display: flex;
+        width: 100%;
+        height: 30%;
+
+        font-family: "HelveticaNeue";
+        font-size: 4rem;
+    }
+
+    
 
     @media screen and (max-width: 480px) {
         .text header {
             font-size: 2rem;
         }
 
-        .buttons #projects{
-            font-size: 0.9rem;
+        .card {
+            --button-fs: 0.9rem;
+        }
+
+        #dc-content {
+            font-size: 14rem;
         }
     }
 
